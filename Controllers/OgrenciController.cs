b14b0f412aea15614c2aa98455c3554813193bf0
Eren Üstündağ <eren.ustundag@tsoft.club>
2024-09-25 14:47:41 +0300

    Öğrenci ekleme, listeleme, güncelleme, silme işlemleri tamamlandı

---
+++ Controllers/OgrenciController.cs
@@ -0,0 +1,119 @@
+using efcoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace efcoreApp.Controllers
+{
+    public class OgrenciController : Controller
+    {
+
+        private readonly DataContext _context;
+
+        public OgrenciController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _context.Ogrenciler.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogrenci model)
+        {
+            _context.Ogrenciler.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit (int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var ogr = await _context.Ogrenciler.FindAsync(id);
+
+            if(ogr == null)
+            {
+                return NotFound();
+            }
+
+            return View(ogr);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit (int id, Ogrenci model)
+        {
+            if(id != model.OgrenciId)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model); // Bu aşamada işaretlenmiş oluyor.
+                    await _context.SaveChangesAsync(); //Bu aşamada veritabanında güncelleme yapılıyor
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    if(!_context.Ogrenciler.Any(o => o.OgrenciId == model.OgrenciId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+    
+    
+        [HttpGet]
+        public async Task<IActionResult> Delete (int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var ogrenci = await _context.Ogrenciler.FindAsync(id);
+
+            if(ogrenci == null)
+            {
+                return NotFound();
+            }
+
+            return View(ogrenci);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete ([FromForm]int id)
+        {
+            var ogrenci = await _context.Ogrenciler.FindAsync(id);
+            if(ogrenci == null)
+            {
+                return NotFound();
+            }
+            _context.Ogrenciler.Remove(ogrenci);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+
+}
